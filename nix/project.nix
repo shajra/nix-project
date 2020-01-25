84cd61331b1b0b593277337b940c9c0b2420fb02
Sukant Hajra <rrudbskr5g@snkmail.com>
2020-01-25 09:30:57 -0600

    allow configuration of `nix-project` target directories

--- nix/project.nix
+++ nix/project.nix
@@ -1,6 +1,7 @@
 { bash
 , coreutils
 , gnugrep
+, gnused
 , gnutar
 , gzip
 , niv
@@ -14,6 +15,8 @@ set -eu
 
 PROG="$(${coreutils}/bin/basename "$0")"
 NIX_EXE="$(command -v nix || true)"
+TARGET_DIR="$(pwd)"
+NIV_DIR="nix"
 TOKEN=~/.config/nix-project/github.token
 COMMAND=
 
@@ -51,9 +54,14 @@ COMMANDS:
 
 OPTIONS:
 
-    -h, --help          print this help message
-    -g, --github-token  file with GitHub API token
-    -N, --nix           filepath of 'nix' executable to use
+    -h, --help             print this help message
+    -t, --target-dir DIR   directory of project to manage
+                           (default: current directory)
+    -S, --source-dir DIR   directory relative to target for
+                           Nix files (default: nix)
+    -g, --github-token     file with GitHub API token (default:
+                           ~/.config/nix-project/github.token)
+    -N, --nix              filepath of 'nix' executable to use
 
     '$PROG' pins all dependencies except for Nix itself,
      which it finds on the path if possible.  Otherwise set
@@ -79,6 +87,20 @@ main()
             ;;
         -n|--niv)
             COMMAND=niv
+            ;;
+        -t|--target-dir)
+            TARGET_DIR="''${2:-}"
+            if [ -z "$TARGET_DIR" ]
+            then die "$1 requires argument"
+            fi
+            shift
+            ;;
+        -S|--sources-dir)
+            NIV_DIR="''${2:-}"
+            if [ -z "$NIV_DIR" ]
+            then die "$1 requires argument"
+            fi
+            shift
             ;;
         -N|--nix)
             NIX_EXE="''${2:-}"
@@ -125,48 +147,78 @@ run()
 {
     if [ "$COMMAND" = niv ]
     then
-        "${niv}/bin/niv" "$@"
+        run_niv "$@"
     elif [ "$COMMAND" = upgrade ]
     then
-        "${niv}/bin/niv" init
-        "${niv}/bin/niv" update
+        niv_init
+        run_niv update
     elif [ "$COMMAND" = scaffold ]
     then
-        "${niv}/bin/niv" init
-        build_scaffold
-        "${niv}/bin/niv" update
+        niv_init
+        add_nix_project
+        install_scripts
+        run_niv update
     else
         die "no command given"
     fi
 }
 
-build_scaffold()
+install_scripts()
 {
-    add_nix_project
-    if ! [ -e support ]
-    then ${coreutils}/bin/mkdir support
+    local support_dir; support_dir="$(target_dir)/support"
+    if ! [ -e "$support_dir" ]
+    then ${coreutils}/bin/mkdir "$support_dir"
     fi
     install_files ${./scaffold/support/docs-generate} \
-        support/docs-generate
-    install_files ${../support/dependencies-upgrade} \
-        support/dependencies-upgrade
-    install_files --mode 644 ${./scaffold}/nix/* nix
-    install_files --mode 644 ${./scaffold}/*.org .
+        "$support_dir/docs-generate"
+    install_files ${./scaffold/support/dependencies-upgrade} \
+        "$support_dir/dependencies-upgrade"
+    ${gnused}/bin/sed -i -e "s/@NIV_DIR@/$NIV_DIR/" "$support_dir"/*
+    install_files --mode 644 ${./scaffold}/nix/* "$(niv_dir)"
+    install_files --mode 644 ${./scaffold}/*.org "$(target_dir)"
 }
 
 add_nix_project()
 {
-    if "${niv}/bin/niv" show \
-        | { ! "${gnugrep}/bin/grep" \
-            --quiet \
-            "github.com/shajra/nix-project"; }
-    then "${niv}/bin/niv" add shajra/nix-project
+    if ! run_niv show nix-project > /dev/null 2>&1
+    then run_niv add shajra/nix-project
     fi
 }
 
 install_files()
 {
     ${coreutils}/bin/install --compare --backup "$@"
+}
+
+niv_init()
+{
+    local dest; dest="$(niv_dir)"
+    local tmp; tmp="$(${coreutils}/bin/mktemp -d)"
+    trap '${coreutils}/bin/rm -rf "'"$tmp"'"' EXIT
+    (
+        cd "$tmp"
+        "${niv}/bin/niv" init
+    ) > /dev/null
+    ${coreutils}/bin/mkdir -p "$dest"
+    ${coreutils}/bin/cp "$tmp/nix/sources.nix" "$dest"
+    if ! [ -e "$dest/sources.json" ]
+    then ${coreutils}/bin/cp "$tmp/nix/sources.json" "$dest"
+    fi
+}
+
+run_niv()
+{
+    "${niv}/bin/niv" --sources-file "$(niv_dir)/sources.json" "$@"
+}
+
+target_dir()
+{
+    ${coreutils}/bin/readlink --canonicalize "$TARGET_DIR"
+}
+
+niv_dir()
+{
+    echo "$(target_dir)/$NIV_DIR"
 }
 
 
