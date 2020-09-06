7f4388c4f7662140c4c9a15ea8f94530be5c127d
Sukant Hajra <rrudbskr5g@snkmail.com>
2020-09-05 22:15:31 -0500

    trade-off boilerplate by using callPackage more

--- nix/default.nix
+++ nix/default.nix
@@ -2,23 +2,26 @@ let
 
     sources = import ./sources-unified.nix;
 
+    buildOverlay = self: super: {
+        niv = (import sources.niv {}).niv;
+        ox-gfm = sources.ox-gfm;
+        inherit nix-project-lib;
+    };
+
     pkgs = import sources.nixpkgs {
         config = {};
-        overlays = [(import ./overlay.nix)];
+        overlays = [
+          (import ./overlay.nix)
+          buildOverlay
+        ];
     };
 
     nix-project-lib = pkgs.recurseIntoAttrs
         (pkgs.callPackage (import ./lib.nix) {});
 
-    nix-project-exe = pkgs.callPackage (import ./project.nix) {
-        niv = (import sources.niv {}).niv;
-        inherit nix-project-lib;
-    };
+    nix-project-exe = pkgs.callPackage (import ./project.nix) {};
 
-    nix-project-org2gfm = pkgs.callPackage (import ./org2gfm.nix) {
-        ox-gfm = sources.ox-gfm;
-        inherit nix-project-lib;
-    };
+    nix-project-org2gfm = pkgs.callPackage (import ./org2gfm.nix) {};
 
 in {
     inherit
